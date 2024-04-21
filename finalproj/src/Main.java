4f0ca9930b6efdd20dcfd47069f9eeccc4709eb1
Alex <alex@unixfy.net>
2024-04-21 02:49:38 +0000

    Add necessary use of Maps/Arrays to Main

--- finalproj/src/Main.java
+++ finalproj/src/Main.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
+    private ArrayList<Flight> flights;
+    private HashMap<User, Service> userBookingMap;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
